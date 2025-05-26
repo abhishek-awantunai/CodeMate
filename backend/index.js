2458c2c8d6bb955083f759f81e42556b23724e7f
Abhishek Kumar <abhishek.kumar@awantunai.com>
2025-05-26 10:48:14 +0530

    initial folders

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-// const URI = "mongodb+srv://byheart2526:U5TxsGd35B91DlDH@namastenode.aeuuy20.mongodb.net/codeMate"
+// const URI = "mongodb+srv://byheart2526:<db_password>@namastenode.aeuuy20.mongodb.net/"
 require("dotenv").config();
 
 const express = require("express");
@@ -6,8 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.use()
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
