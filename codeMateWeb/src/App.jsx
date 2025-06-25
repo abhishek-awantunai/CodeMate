a27f9c7835a2623019ee238f4d0d15dc2358b132
Abhishek Kumar <abhishek.kumar2526@gmail.com>
2025-06-25 20:54:13 +0530

    updated changes

--- codeMateWeb/src/App.jsx
+++ codeMateWeb/src/App.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import Login from './pages/Login';
-import Feed from './pages/Feed';
-import About from './pages/About';
-import Privacy from './pages/Privacy';
-import Help from './pages/Help';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
-import ProtectedRoute from './components/ProtectedRoute';
-import Chat from './pages/Chat';
-import Connections from './pages/Connections';
-import Playground from './pages/Playground';
-import Memo from './pages/Memo';
-import Resume from './pages/Resume';
-import Algo from './pages/Algo';
-import Akshay from './pages/Akshay';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import About from "./pages/About";
+import Privacy from "./pages/Privacy";
+import Help from "./pages/Help";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Chat from "./pages/Chat";
+import Connections from "./pages/Connections";
+import Playground from "./pages/Playground";
+import Memo from "./pages/Memo";
+import Resume from "./pages/Resume";
+import Algo from "./pages/Algo";
+import Akshay from "./pages/Akshay";
 
 function App() {
   return (
