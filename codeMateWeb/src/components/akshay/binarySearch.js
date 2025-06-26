81c3e39b4c237ea81e82cefc8e509fea462611b2
Abhishek Kumar <abhishek.kumar2526@gmail.com>
2025-06-26 06:19:52 +0530

    fix: correct typos and formatting in binarySearch.js

--- codeMateWeb/src/components/akshay/binarySearch.js
+++ codeMateWeb/src/components/akshay/binarySearch.js
@@ -1,5 +1,4 @@
 const binarySearch = (arr, num) => {
-
     // initialize pointers to traverse in the array
     let start = 0;
     let end = arr.length - 1;
@@ -11,7 +10,7 @@ const binarySearch = (arr, num) => {
     // calculate midpoint of the array
     let mid = Math.floor((start + end) / 2);
 
-    // loop ovet and find element and return the index of the element
+    // loop over and find element and return the index of the element
     while (start !== mid && end !== mid) {
         if (num > arr[mid]) {
             start = mid;
@@ -20,12 +19,11 @@ const binarySearch = (arr, num) => {
         } else {
             return mid;
         }
-        mid = Math.floor((start + end) / 2)
+        mid = Math.floor((start + end) / 2);
     }
-
 
     // by default return -1
     return -1;
-}
+};
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
